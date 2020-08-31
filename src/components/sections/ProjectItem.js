f4cfc0cff6514fbc6027ab4a817360da9df600dd
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-09-01 02:32:15 +1000

    Refactored and Improved Project Component
    - Created data folder containing a projects.json file with all of the project details.
    - Updated GraphQL query to collect data from the projects.json file.
    - Created gatsby-node.js and created an export in order to successfully load data via the GraphQL query.
    - Refactored project cards from Projects into an individual ProjectItem component.

---
+++ src/components/sections/ProjectItem.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Img from "gatsby-image"
+export default function ProjectItem({
+  title,
+  image,
+  image_alt,
+  description,
+  githubURL,
+}) {
+  return (
+    <div className="project-item">
+      <figure className="image mb-3">
+        <Img fluid={image.childImageSharp.fluid} alt={image_alt} />
+        {/* <img src={githubFinderIMG} alt="" /> */}
+      </figure>
+      <p className="title is-size-4 has-text-centered">{title}</p>
+      <p className="has-text-centered subtitle is-size-6">{description}</p>
+      <a
+        className="button is-dark is-fullwidth  has-text-weight-semibold"
+        href={githubURL}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        View on GitHub
+      </a>
+    </div>
+  )
+}
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,35 +1,25 @@
 import React from "react"
-import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
-
+import ProjectItem from "./ProjectItem"
 export default function Projects() {
   const data = useStaticQuery(
     graphql`
-      fragment fluidImage on File {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
       query {
-        imageOne: file(relativePath: { eq: "githubFinder.png" }) {
-          ...fluidImage
-        }
-        imageTwo: file(relativePath: { eq: "wearitReact.png" }) {
-          ...fluidImage
-        }
-        imageThree: file(relativePath: { eq: "wearitEJS.png" }) {
-          ...fluidImage
-        }
-        imageFour: file(relativePath: { eq: "revueMongoDB.png" }) {
-          ...fluidImage
-        }
-        imageFive: file(relativePath: { eq: "revueSQLite.png" }) {
-          ...fluidImage
-        }
-        imageSix: file(relativePath: { eq: "portfolio.png" }) {
-          ...fluidImage
+        allProject {
+          nodes {
+            id
+            title
+            githubURL
+            description
+            image_alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 1000) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
         }
       }
     `
@@ -42,81 +32,9 @@ export default function Projects() {
       <div className="columns is-centered">
         <div className="column is-four-fifths">
           <div className="project-grid">
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageOne.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">GitHub Finder</p>
-              <p className="has-text-centered subtitle is-size-6">
-                React website that uses the GitHub API to search for users and
-                display information from their profile.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageTwo.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">WearIt React</p>
-              <p className="has-text-centered subtitle is-size-6">
-                Clothing store website built with React featuring a contact form
-                and newsletter signup via Mailchimp.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageThree.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">WearIt EJS</p>
-              <p className="has-text-centered subtitle is-size-6">
-                Clothing store website with a dark mode toggle, built with EJS
-                and Express.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageFour.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">ReVue MongoDB</p>
-              <p className="has-text-centered subtitle is-size-6">
-                Music review website built with Vue and MongoDB.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageFive.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">ReVue SQLite</p>
-              <p className="has-text-centered subtitle is-size-6">
-                Music review website built with Vue and SQLite.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
-            <div className="project-item">
-              <figure className="image mb-3">
-                <Img fluid={data.imageSix.childImageSharp.fluid} />
-              </figure>
-              <p className="title is-size-4 has-text-centered">Portfolio</p>
-              <p className="has-text-centered subtitle is-size-6">
-                Personal portfolio website built with Gatsby and Bulma.
-              </p>
-              <button className="button is-dark is-fullwidth  has-text-weight-semibold">
-                GitHub Repository
-              </button>
-            </div>
+            {data.allProject.nodes.map(project => (
+              <ProjectItem {...project} key={project.title} />
+            ))}
           </div>
         </div>
       </div>
