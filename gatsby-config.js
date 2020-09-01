392dca72f47e361005b4d4be08cb52a75fad439b
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-09-01 23:20:50 +1000

    Added SEO Component
    - Created SEO component and imported into topages.
    - Added description to siteMetadata in gatsby-config.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: "Abymar",
+    description: "Personal portfolio site.",
   },
   plugins: [
     `gatsby-plugin-preact`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import Projects from "../components/sections/Projects"
 import About from "../components/sections/About"
 import Contact from "../components/sections/Contact"
 import Hero from "../components/sections/Hero"
+import SEO from "../components/SEO"
 export default function Home() {
   return (
     <Layout>
+      <SEO title="Home" />
       <Hero />
       <Projects />
       <About />
