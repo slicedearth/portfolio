a59328a258a26b1350bc3d541721a150cc335182
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-09-03 00:39:37 +1000

    Created Contact Page, Added Analytics and Updated Project URLs
    - Created separate contact page and renamed Contact component to ContactForm.
    - Updated contact links in hero and header components.
    - Installed gatsby-plugin-google-analytics.
    - Updated GitHub URLs in projects.json.
    - Added has-text-white class to Hero buttons.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import Projects from "../components/sections/Projects"
 import About from "../components/sections/About"
-import Contact from "../components/sections/Contact"
 import Hero from "../components/sections/Hero"
 import SEO from "../components/SEO"
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <Hero />
       <Projects />
       <About />
-      <Contact />
     </Layout>
   )
 }
